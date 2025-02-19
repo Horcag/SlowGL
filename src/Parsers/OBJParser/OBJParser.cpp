f90157de248aee3f0737c43c1ac7a37461570b3e
Igor <igorvlasov732@gmail.com>
2025-02-19 19:28:41 +0400

    Parser_features_fix

--- src/Parsers/OBJParser/OBJParser.cpp
+++ src/Parsers/OBJParser/OBJParser.cpp
@@ -89,9 +89,40 @@ std::vector<Face> OBJParser::parse_faces(std::ifstream &file) const {
 
 Model3D OBJParser::parse(std::ifstream &file) const {
     Model3D model;
-    model.set_vertex(parse_vertex(file));
-    model.set_vertex_texture(parse_vertex_texture(file));
-    model.set_vertex_normal(parse_vertex_normal(file));
-    model.set_faces(parse_faces(file));
+    std::vector<sf::Vector3f> vertex;
+    std::vector<sf::Vector2f> vertex_texture;
+    std::vector<sf::Vector3f> vertex_normal;
+    std::vector<Face> faces;
+    Face face{};
+    std::string line;
+    double x, y, z;
+    while (std::getline(file, line)) {
+        std::istringstream lineStream(line);
+        std::string pref;
+        lineStream >> pref;
+        if (pref == "v") {
+            lineStream >> x >> y >> z;
+            vertex.emplace_back(x, y, z);
+        } else if (pref == "vt") {
+            lineStream >> x >> y;
+            vertex_texture.emplace_back(x, y);
+        } else if (pref == "vn") {
+            lineStream >> x >> y >> z;
+            vertex_normal.emplace_back(x, y, z);
+        } else if (pref == "f") {
+            for (int i = 0; i < 3; ++i) {
+                std::string faceData;
+                lineStream >> faceData;
+                std::replace(faceData.begin(), faceData.end(), '/', ' ');
+                std::istringstream faceDataStream(faceData);
+                faceDataStream >> face.vertexIndices[i] >> face.textureIndices[i] >> face.normalIndices[i];
+            }
+            faces.push_back(face);
+        }
+    }
+    model.set_vertex(vertex);
+    model.set_vertex_texture(vertex_texture);
+    model.set_vertex_normal(vertex_normal);
+    model.set_faces(faces);
     return model;
 }
