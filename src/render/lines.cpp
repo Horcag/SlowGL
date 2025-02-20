6667027079e66b8c9b430979f28b975892676480
Nikita <rachet337@gmail.com>
2025-02-20 12:34:02 +0400

    add dotted_line

--- src/render/lines.cpp
+++ src/render/lines.cpp
@@ -1,6 +1,14 @@
 #include "../Image/SFMLImage.h"
+#include "cmath"
 namespace sgl::render {
-    void dotted_line() {
 
-    }
-}
+void draw_dotted_line(sgl::SFMLImage& image, sf::Vector2u start, sf::Vector2u end, sf::Color color, uint count) {
+  double step = 1.0 / count;
+  for (double i = 0; i < 1; i += step) {
+    sf::Vector2u a = {
+        static_cast<unsigned int>(std::round(start.x * (1 - i) + end.x * i)),
+        static_cast<unsigned int>(std::round(start.y * (1 - i) + end.y * i))
+    };
+    image.setPixel(a, color);
+  }
+}}
