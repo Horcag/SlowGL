84cafba5cc8740e58a5d0ea69bbba1ef31d97c59
Nikita <rachet337@gmail.com>
2025-02-20 13:20:31 +0400

    add line_fix1

--- src/render/lines.cpp
+++ src/render/lines.cpp
@@ -1,20 +1,34 @@
 #include "../Image/SFMLImage.h"
 #include "cmath"
 
+unsigned int linear_interpolation(const unsigned int start, const unsigned int end, const double t) {
+    return static_cast<unsigned int>(std::round(start * (1 - t) + end * t));
+}
+
 namespace sgl::render {
-    void draw_dotted_line(sgl::SFMLImage&image, sf::Vector2u start, sf::Vector2u end, sf::Color color,
-                          unsigned int count) {
-        double step = 1.0 / count;
+    void draw_dotted_line(sgl::SFMLImage&image, const sf::Vector2u start, const sf::Vector2u end, const sf::Color color,
+                          const unsigned int count) {
+        const double step = 1.0 / count;
         for (double i = 0; i < 1; i += step) {
-            sf::Vector2u a = {
-                static_cast<unsigned int>(std::round(start.x * (1 - i) + end.x * i)),
-                static_cast<unsigned int>(std::round(start.y * (1 - i) + end.y * i))
+            const sf::Vector2u a = {
+                linear_interpolation(start.x, end.x, i),
+                linear_interpolation(start.y, end.y, i)
             };
             image.setPixel(a, color);
         }
     }
 
-    void draw_dotted_line_fix1(sgl::SFMLImage&image, sf::Vector2u start, sf::Vector2u end, sf::Color color) {
+    void draw_dotted_line_fix1(sgl::SFMLImage&image, const sf::Vector2u start, const sf::Vector2u end,
+                               const sf::Color color) {
+        const double count = std::sqrt(std::pow(start.x - end.x, 2) + std::pow(start.y - end.y, 2));
+        const double step = 1.0 / count;
+        for (double i = 0; i < 1; i += step) {
+            const sf::Vector2u a = {
+                linear_interpolation(start.x, end.y, i),
+                linear_interpolation(start.y, end.y, i)
+            };
+            image.setPixel(a, color);
+        }
     }
 
     void draw_dotted_line_fix2(sgl::SFMLImage&image, sf::Vector2u start, sf::Vector2u end, sf::Color color) {
